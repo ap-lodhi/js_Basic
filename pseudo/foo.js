51908b588ae39351f15228ad3b03640a7fd28be7
ap-lodhi <anilpatel.bcs@gmail.com>
2023-03-16 13:49:31 +0530

    first commit

---
+++ pseudo/foo.js
@@ -0,0 +1,15 @@
+foo()
+function foo(){
+    console.log('heloo')
+}
+
+
+
+const arr=[1,3,4,2,]
+arr[0]=8
+console.log(arr)
+
+
+console.log(name)
+name ='masai'
+var  name
---
+++ pseudo/option.js
@@ -0,0 +1,14 @@
+// (function(){console.log('heloo')})
+
+
+// function(){console.log('heloo')}()  //required functon name  syntax error
+
+(function(){console.log('heloo')})(); // it  will print hello
+
+// function(){console.log('heloo')}; //required functon name  syntax
+
+
+console.log(name)  // undefined
+var name ='ANIL'
+
+
